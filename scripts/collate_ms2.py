603d72f2f12413d8288f88932a9345b6ec261665
Michał Startek <michal.startek@mimuw.edu.pl>
2022-01-22 07:48:52 +0100

    Improvements

--- scripts/collate_ms2.py
+++ scripts/collate_ms2.py
@@ -5,20 +5,32 @@ from pathlib import Path
 import shutil
 from tqdm import tqdm
 from collections import Counter, defaultdict
-
-
-parser = argparse.ArgumentParser(description="Flatten a group of scans")
-parser.add_argument("src", metavar="<source.d>", type=Path, help="Source TDF directory")
-parser.add_argument("dst", metavar="<destination.d>", type=Path, help="Output directory")
-parser.add_argument("-s", "--silent", action="store_true", help="Silent (do not show progressbar)")
-parser.add_argument("-f", "--force", action="store_true", help="Delete the target directory if it exists")
-args = parser.parse_args()
-
 from opentimspy import OpenTIMS
 from savetimspy import SaveTIMS
 
 
+parser = argparse.ArgumentParser(description="Flatten a series MS2 MIDIA scans into MS1. Frames from each MS2 cycle are summed up into a single MS1 frame.")
+parser.add_argument("src", metavar="<source.d>", type=Path, help="Source TDF directory")
+parser.add_argument("dst", metavar="<destination.d>", type=Path, help="Output directory")
+parser.add_argument("-s", "--silent", action="store_true", help="Silent (do not show progressbar)")
+parser.add_argument("-f", "--force", action="store_true", help="Delete the target directory if it exists")
+parser.add_argument("-c", "--cycles", help="Comma-separated list MIDIA cycles or cycle ranges to extract. Example: 314,340-350,356. Extract everything if omitted.", default="")
+args = parser.parse_args()
+
 assert args.src.is_dir()
+
+cycles = set()
+for cycle_desc in args.cycles.split(","):
+    if '-' in cycle_desc:
+        start, end = cycle_desc.split('-')
+        cycles.update(range(int(start), int(end)+1))
+    else:
+        cycles.add(int(cycle_desc))
+
+def extract_cycle(cycle_id):
+    if len(cycles) == 0:
+        return True
+    return cycle_id in cycles
 
 progressbar = (lambda x: x) if args.silent else tqdm
 
@@ -44,18 +56,22 @@ max_frame = max(groups.keys())
 
 collected = None
 
+ms1_id = -1
+cycle_id = 0
+
+
 for frame_id in progressbar(range(1, max_frame+2)):
     if not frame_id in groups:
         if collected is not None:
-            collected.sort()
-            scan, tof, intens = zip(*collected)
-            s.save_frame_tofs(scan, tof, intens, frame_to_scans[frame_id-1], copy_sql=ms1_id)
-            pass
+            if extract_cycle(cycle_id):
+                collected.sort()
+                scan, tof, intens = zip(*collected)
+                s.save_frame_tofs(scan, tof, intens, frame_to_scans[frame_id-1], copy_sql=ms1_id)
+            cycle_id += 1
         ms1_id = frame_id
         collected = []
     else:
-        D = ot.query(frame_id, columns="scan tof intensity".split())
-        n_scans = frame_to_scans[frame_id]
-        collected.extend(zip(D['scan'], D['tof'], D['intensity']))
-
-
+        if extract_cycle(cycle_id):
+            D = ot.query(frame_id, columns="scan tof intensity".split())
+            n_scans = frame_to_scans[frame_id]
+            collected.extend(zip(D['scan'], D['tof'], D['intensity']))
