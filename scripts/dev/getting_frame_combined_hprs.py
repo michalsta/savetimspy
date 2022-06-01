809ad9bcf68a56d82ef41585fe54f4706e7a51a6
matteo <matteo.lacki@gmail.com>
2022-06-01 19:54:24 +0200

    almost finished a new hpr iterator

--- scripts/dev/getting_frame_combined_hprs.py
+++ scripts/dev/getting_frame_combined_hprs.py
@@ -39,7 +39,7 @@ target = project_folder/"tests"/"hprs"/source.name
 
 HPR_intervals = make_overlapping_HPR_mz_intervals(
     min_mz=400,
-    max_mz=413,
+    max_mz=426,
 )
 
 hprs = HPRS(
@@ -49,20 +49,91 @@ hprs = HPRS(
 )
 
 
-hprs.plot_scans_and_steps_used_by_hypothetical_precursor_range(hpr_idx=0)
-it = iter(hprs)
+# hprs.plot_scans_and_steps_used_by_hypothetical_precursor_range(hpr_idx=0)
+hprs.step_to_scan_hpr_dfs
 
-it = hprs.full_iter()
-hpr_byte = next(it)
-print(hpr_byte.cycle, hpr_byte.step, hpr_byte.hpr_idx)
-len(hpr_byte.intensities)
+# lets try to rewrite the iterator
+import numba
+import numpy as np
+
+cols_to_choose = ("scan","tof","intensity")
+tof_intensity = ["tof","intensity"]
+# cycle_step_tuples = zip(
+#     hprs.dia_run.DiaFrameMsMsInfo.cycle, 
+#     hprs.dia_run.DiaFrameMsMsInfo.step,
+# )
+cycle = 1
 
 
-df = pd.DataFrame({
-    "scan":         hpr_byte.scans,
-    "tof":          hpr_byte.tofs,
-    "intensity":    hpr_byte.intensities
-})
-df
+# get all MS2 peaks per cycle
+
+ms2_frame_ids_per_cycle = hprs.dia_run.cycle_step_to_ms2_frame(
+    cycle=cycle,
+    step=np.arange(hprs.dia_run.min_step, hprs.dia_run.max_step+1)
+)
+raw_peaks_per_cycle = hprs.dia_run._get_frames_raw(
+    frame_ids=ms2_frame_ids_per_cycle,
+    columns=("frame","scan","tof","intensity"),
+)
+raw_peaks_per_cycle['step'] = hprs.dia_run.ms2_frame_to_step(raw_peaks_per_cycle["frame"])
+del raw_peaks_per_cycle['frame']
+raw_peaks_per_cycle = pd.DataFrame(raw_peaks_per_cycle)
+raw_peaks_per_cycle = raw_peaks_per_cycle.set_index(["step", "scan"])
+
+# need to make something like 
+hprs.hpr_quadrupole_matches
+
+# this one time:
+step_to_scan_hpr_df = [ ]
+for step, df in enumerate(hprs.step_to_scan_hpr_dfs):
+    df = df.reset_index()
+    df["step"] = step
+    step_to_scan_hpr_df.append(df)
+step_to_scan_hpr_df = pd.concat(step_to_scan_hpr_df, ignore_index=True)
+step_to_scan_hpr_df = step_to_scan_hpr_df.set_index(["step", "scan"])
+
+
+raw_peaks_prod = pd.merge(
+    step_to_scan_hpr_df,
+    raw_peaks_per_cycle,
+    left_index=True,
+    right_index=True,
+)
+# figure out what should be here
+for hpr_idx, data in raw_peaks_prod.groupby("hpr_idx")[tof_intensity]:
+
+
+
+
+# it = iter(hprs)
+# hprs.dia_run.DiaFrameMsMsInfo
+# hpr_byte = next(it)
+# print(hpr_byte.cycle, hpr_byte.step, hpr_byte.hpr_idx)
+
+
+# datasets = [[] for _ in range(len(hprs.HPR_intervals))]
+# prev_cycle = 0
+# for hpr_byte in hprs:
+
+#     if prev_cycle < hpr_byte.cycle:
+#         prev_cycle = hpr_byte.cycle
+#         datasets = merge_data(datasets)
+#         for dataset in datasets:
+
+#         for _lst in hpr_2_step_data_per_cycle:
+#             _lst.clear()
+
+
+# it = hprs.full_iter()
+# hpr_byte = next(it)
+# len(hpr_byte.intensities)
+
+
+# df = pd.DataFrame({
+#     "scan":         hpr_byte.scans,
+#     "tof":          hpr_byte.tofs,
+#     "intensity":    hpr_byte.intensities
+# })
+# df
 
 
